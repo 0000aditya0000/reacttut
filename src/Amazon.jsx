ad30574d6e6a63f07a6319ab3d2e69d0c8fc01d9
0000aditya0000 <60063610+0000aditya0000@users.noreply.github.com>
2023-12-14 10:46:18 +0530

    Update Amazon.jsx

--- src/Amazon.jsx
+++ src/Amazon.jsx
@@ -12,6 +12,13 @@ const Amazon = () =>{
                     sname = {Sdata[3].sname}
                     link = {Sdata[3].link}
                 />
+        <Card 
+                    key = {Sdata[3].key}
+                    imgsrc = {Sdata[3].imgsrc}
+                    title = {Sdata[3].title}
+                    sname = {Sdata[3].sname}
+                    link = {Sdata[3].link}
+                />
     )
 }
 
